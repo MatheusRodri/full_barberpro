2dd10d6646572cbf351200341999c8f722fb2467
Matheus <matheus.rj25@hotmail.com>
2022-12-26 19:07:19 -0300

    detail user and create middleware isAuthenticated

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,11 +1,14 @@
 import { CreateUserController } from './controllers/user/CreateUserController';
 import {Router,Response,Request} from 'express';
 import { AuthUserController } from './controllers/user/AuthUserController';
+import { DetailUserController } from './controllers/user/DetailUserController';
+import {isAuthenticated} from "./middlewares/isAuthenticated";
 
 const router = Router();
 
 router.post('/users',new CreateUserController().handle)
 router.post('/session',new AuthUserController().handle);
+router.get('/me',isAuthenticated, new DetailUserController().handle);
 
 
 export {router};
