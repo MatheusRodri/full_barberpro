300d0592e4f9a5179d9efe22db844dfad90aa261
Matheus <matheus.rj25@hotmail.com>
2022-12-26 19:23:01 -0300

    update user

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -3,12 +3,14 @@ import {Router,Response,Request} from 'express';
 import { AuthUserController } from './controllers/user/AuthUserController';
 import { DetailUserController } from './controllers/user/DetailUserController';
 import {isAuthenticated} from "./middlewares/isAuthenticated";
+import {UpdateUserController} from "./controllers/user/UpdateUserController.";
 
 const router = Router();
 
 router.post('/users',new CreateUserController().handle)
 router.post('/session',new AuthUserController().handle);
 router.get('/me',isAuthenticated, new DetailUserController().handle);
+router.put('/users',isAuthenticated, new UpdateUserController().handle);
 
 
 export {router};
